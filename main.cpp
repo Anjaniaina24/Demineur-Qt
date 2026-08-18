810054fefb3f901b00d99d3369dd60c6ed624cb0
Anjaniaina24 <nirinjanaharyanjaniaina@gmail.com>
2026-08-18 13:37:47 +0300

    test

--- main.cpp
+++ main.cpp
@@ -1,9 +1,13 @@
+#include <QApplication>
+#include <QMainWindow>
 
-
-int main(int(argc, char *argv))
+int main(int argc, char *argv[]) 
 {
 
+    QApplication app(argc, argv);
+    QMainWindow window;
+    window.resize(800, 600);
+    window.show();
     
-
-    return 0;
+    return app.exec();
 }
